6f40101b22300310521143bf95fc4ea328061df5
soyambhu <85630358+soyambhu@users.noreply.github.com>
2022-04-29 15:37:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function preload()
 
 function setup() {
 	canvas =  createCanvas(600, 500);
-	canvas.position(600,500);
+	canvas.position(600,650);
 
 	video = createCapture(VIDEO);
 	video.hide();
