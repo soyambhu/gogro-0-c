1ebc287be51c8d2bff5d4f95090f8ca55a93f62c
soyambhu <85630358+soyambhu@users.noreply.github.com>
2022-04-30 15:42:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,31 @@
 song = "";
-leftWristX= 0;
-leftWristY= 0;
-rightWristX= 0;
-rightWristY= 0;
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 function preload()
 {
 	song = loadSound("music.mp3");
 }
 
-
 function setup() {
-	canvas =  createCanvas(600, 550);
+	canvas =  createCanvas(600, 500);
 	canvas.center();
 
 	video = createCapture(VIDEO);
 	video.hide();
 
 	poseNet = ml5.poseNet(video, modelLoaded);
-	poseNet.on('Pose', gotPoses);
+	poseNet.on('pose', gotPoses);
 }
-
-
 
 function modelLoaded() {
-	console.log('PoseNet Is Initialized');
+  console.log('PoseNet Is Initialized');
 }
 
-
-
-
-function play()
-{
-	song.play();
-	song.setVolume(1);
-	song.rate(1);
-}
 function gotPoses(results)
 {
   if(results.length > 0)
@@ -54,23 +44,29 @@ function gotPoses(results)
 		
   }
 }
+
 function draw() {
 	image(video, 0, 0, 600, 500);
 
-    Fill("#FF0000");
-    stroke("#FF0000");
-    
-    if(scoreLeftWrist > 0.2)
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	
+	if(scoreLeftWrist > 0.2)
 	{
 		circle(leftWristX,leftWristY,20);
 		InNumberleftWristY = Number(leftWristY);
 		new_leftWristY = floor(InNumberleftWristY *2);
 		leftWristY_divide_1000 = new_leftWristY/1000;
-        Volume = leftWristY_divide_1000 *2 ;
 		document.getElementById("volume").innerHTML = "Volume = " + leftWristY_divide_1000;		
 		song.setVolume(leftWristY_divide_1000);	
-    }
+	}
+
 }
 
-
-
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
